6c78926d54942dc2b37ee77b5e372e9e9ed33e2b
bintang2629 <bimbimpratama23@gmail.com>
2026-04-21 15:49:08 +0700

    Remove Mahasiswa class implementation
    Removed the implementation of the Mahasiswa class and main function.

--- ParadigmaOOP_0072.cpp
+++ ParadigmaOOP_0072.cpp
@@ -2,21 +2,5 @@
 using namespace std;
 
 class Mahasiswa{
-    public:
-        int nim;
-        string nama;
-        float nilai;
-        void printData(){
-            cout << "NIM: " << nim << endl;
-            cout << "Nama: " << nama << endl;
-            cout << "Nilai: " << nilai << endl;
-        }
-    };
-    int main(){
-        Mahasiswa mhs1;
-
-        mhs1.nim = 1111;
-        mhs1.nama = "Joko susilo";
-        mhs1.nilai = 75.5;
-        mhs1.printData();
+ 
     }
